ef2df4ae98dd543414bb67f4b7e50c7893502082
jdromero88 <jdromerov88@gmail.com>
2024-08-30 18:30:00 -0400

    Update algolia script

--- update-algolia.js
+++ update-algolia.js
@@ -4,9 +4,12 @@ const client = algoliasearch('$app_key', '$api_key')
 
 const index = client.initIndex('india_states')
 
-await fetch('https://indian-states-v2.netlify.app/algolia.json')
-  .then((response) => response.json())
+let response = await fetch('https://indian-states-v2.netlify.app/algolia.json')
+
+if ( response?.ok ) {
+  data = response.json()
   // .then( data => console.log(data))
-  .then((data) =>
-    index.saveObjects(data, { autoGenerateObjectIDIfNotExist: true })
-  )
+  index.saveObjects(data, { autoGenerateObjectIDIfNotExist: true })
+} else {
+  console.log('some error ocurred', response.json())
+}
