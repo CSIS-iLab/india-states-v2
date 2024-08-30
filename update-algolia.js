44dc137bcf773c2bc4b886afd35028de4785d298
jdromero88 <jdromerov88@gmail.com>
2024-08-30 18:35:39 -0400

    Update the algolia script language

--- update-algolia.js
+++ update-algolia.js
@@ -4,11 +4,10 @@ const client = algoliasearch('$app_key', '$api_key')
 
 const index = client.initIndex('india_states')
 
-let response = await fetch('https://indian-states-v2.netlify.app/algolia.json')
+let response = await fetch('https://indianstates.csis.org/algolia.json')
 
 if ( response?.ok ) {
   let data = response.json()
-  // .then( data => console.log(data))
   index.saveObjects(data, { autoGenerateObjectIDIfNotExist: true })
 } else {
   console.log('some error ocurred', response.json())
